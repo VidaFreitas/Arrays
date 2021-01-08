fade63ce8c4fa5e43e87a4f26e661df81eabb289
Vida Valessa <vidavalessa65@gmail.com>
2021-01-08 11:31:24 -0300

    Arrays list

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            var meuArray = new int[5] { 10, 12, 14, 16, 18 };
+            meuArray[0] = 12;
+
+            Console.WriteLine(meuArray[0]);
+            Console.WriteLine(meuArray[1]);
+            Console.WriteLine(meuArray[2]);
+            Console.WriteLine(meuArray[3]);
+            Console.WriteLine(meuArray[4]);
+        }
+    }
+}
